a5903c8c51e33092fb520ba2548fb259b73a2a10
om-gujarathi-noovosoft <om.gujarathi@noovosoft.com>
2022-11-14 16:05:24 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/pushnotification/PushNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pushnotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PushNotificationApplication
+
+fun main(args: Array<String>) {
+    runApplication<PushNotificationApplication>(*args)
+}
