5f2fa4698a8792d53f6846cadebe721f80e043c0
mayurkidi <mayurkidi77@gmail.com>
2022-11-15 16:31:50 +0530

    update code for threads

--- src/main/kotlin/com/example/pushnotification/services/EmailSender.kt
+++ src/main/kotlin/com/example/pushnotification/services/EmailSender.kt
@@ -1,6 +1,5 @@
 package com.example.pushnotification.services
 
-import com.example.pushnotification.models.ExecutionStatus
 import com.example.pushnotification.models.Notification
 import com.example.pushnotification.repositories.NotificationRepository
 import org.springframework.scheduling.annotation.Scheduled
@@ -10,14 +9,36 @@ import org.springframework.stereotype.Component
 class EmailSender(val notificationService: NotificationService, val notificationRepository: NotificationRepository) {
     val tasks: MutableList<Notification> = notificationService.fetchNewData().toMutableList()
 
-    @Scheduled(fixedRate = 2000)
-    fun scheduleTaskWithFixedRate() {
+    @Scheduled(fixedRate = 1000)
+    fun scheduleTaskWithFixedRateT1() {
         if (tasks.size > 30) {
             val currentTask = tasks.first()
             currentTask.executionStatus = notificationService.sendSimpleMail(currentTask)
             notificationRepository.save(currentTask)
+            tasks.removeAt(0)
+            println("T1")
+
         } else {
             tasks.addAll(notificationService.fetchNewData())
+            if (tasks.size < 30) {
+                Thread.sleep(10000)
+            }
+        }
+    }
+
+    @Scheduled(fixedRate = 1000)
+    fun scheduleTaskWithFixedRateT2() {
+        if (tasks.size > 30) {
+            val currentTask = tasks.first()
+            currentTask.executionStatus = notificationService.sendSimpleMail(currentTask)
+            notificationRepository.save(currentTask)
+            tasks.removeAt(0)
+            println("T2")
+        } else {
+            tasks.addAll(notificationService.fetchNewData())
+            if (tasks.size < 30) {
+                Thread.sleep(10000)
+            }
         }
     }
 }
